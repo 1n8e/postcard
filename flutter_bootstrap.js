03aec381683966e9426dc5c320bf7ba7fe689f79
ogayvladislav47@gmail.com <chebupeli>
2025-03-08 15:27:58 +0500

    some fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3690361318"
+    serviceWorkerVersion: "821697977"
   }
 });
